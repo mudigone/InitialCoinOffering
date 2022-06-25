bcb6caeb72ed4761c8f6ac1bd481538e3cac4f3c
mudigone <76899523+mudigone@users.noreply.github.com>
2022-06-25 20:32:45 +0500

    ICO commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x27f8814F61aff1791b0B182edf4aB7396339b575";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
